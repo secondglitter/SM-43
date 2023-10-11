1ca6f23e242d33b0e35f304e455693a866879515
Joshua Gonzalez Romero <69594954+secondglitter@users.noreply.github.com>
2023-10-11 01:17:41 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Inicio from './pagina/inicio';
+import Dashboard from './pagina/dashboard';
+import ERROR from './componentes/404';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Inicio/>
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard/>
+    },
+    {
+      path: "/404",
+      element: <ERROR/>
+    }
+  ])
+  return(
+    <>
+    <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App;
